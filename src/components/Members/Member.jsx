fc0d905d9ae5b63533e24f8f9cb70f716b14a5d3
Amr <amr70210@gamil.com>
2023-10-26 16:10:49 +0300

    u

--- src/components/Members/Member.jsx
+++ src/components/Members/Member.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import styled from "styled-components";
 
 const Flex = styled.div`
-    display: flex;
-    gap: 5px;
-    align-items: center;
-    justify-content: ${(props) => props.justify};
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  justify-content: ${(props) => props.justify};
 `;
 
 const Span = styled.span`
-    white-space: nowrap;
-    font-weight: bold;
-    color:${(props) =>
-        props.status === "online" ? "var(--light)" : "var(--semi-dark)"};;
+  white-space: nowrap;
+  font-weight: bold;
+  color: ${(props) =>
+    props.status === "online" ? "var(--light)" : "var(--semi-dark)"};
 `;
 
 // const Status = styled.div`
@@ -24,19 +24,23 @@ const Span = styled.span`
 // `;
 
 const Img = styled.img`
-    display: ${(props)=> props.status === 'online' ? 'inline-flex' : 'none'};
-    width: 30px;
-    height: 30px;
-    border-radius: 50%;
+  display: ${(props) => (props.status === "online" ? "inline-flex" : "none")};
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  @media (max-width: 769px) {
+    width: 20px;
+    height: 20px;
+  }
 `;
 export default function Member({ img, name, status, email }) {
-    return (
-        <Flex justify={"space-between"} key={email}>
-            <Flex>
-                <Img src={img} status={status}></Img>
-                <Span status={status}>{name}</Span>
-                {/* <Status status={status}></Status> */}
-            </Flex>
-        </Flex>
-    );
+  return (
+    <Flex justify={"space-between"} key={email}>
+      <Flex>
+        <Img src={img} status={status}></Img>
+        <Span status={status}>{name}</Span>
+        {/* <Status status={status}></Status> */}
+      </Flex>
+    </Flex>
+  );
 }
