33d94cd31ffe8ac43baa09b1aaa19ccb5f44600c
Amr <amr70210@gamil.com>
2023-10-27 20:31:48 +0200

    u

--- src/components/Members/MembersIcon.jsx
+++ src/components/Members/MembersIcon.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import styled from "styled-components";
 
 const I = styled.i`
-    float:right;
-    margin-right:1rem;
-    font-size: 2rem;
+  float: right;
+  margin-right: 1rem;
+  font-size: 2rem;
+  @media (max-width: 768px) {
+    font-size: 1rem;
+    margin-right: 0.5rem;
+  }
 `;
 
 export default function MembersIcon({ membersWindow, toggleMembersWindow }) {
-    return (
-        <I
-            className={membersWindow ? "bi bi-people-fill" : "bi bi-people"}
-            onClick={toggleMembersWindow}
-        ></I>
-    );
+  return (
+    <I
+      className={membersWindow ? "bi bi-people-fill" : "bi bi-people"}
+      onClick={toggleMembersWindow}
+    ></I>
+  );
 }
